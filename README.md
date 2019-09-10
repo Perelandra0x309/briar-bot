80dcf7f126274aec180b580a08e0f76b0f6e1613
Brian Hill <supernova@tycho.email>
2019-09-09 20:49:25 -0400

    Initial proof of concept

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# briar-bot
-Control a device remotely through Briar
+# BriarBot
+Briar Bot will run commands sent from a remote Briar contact to a [briar-headless](https://code.briarproject.org/briar/briar/tree/master/briar-headless) instance running on a PC.  The resulting output of the command is returned to the contact as a message.
+
+Project status: Proof of concept
+
+Current development environment: Linux Mint 19.1, Eclipse IDE, Gradle
+
+##Using BriarBot
+First you need to have briar-headless running and add a contact to it from another device running Briar.  You can follow the instruction on the briar-headless Github page to get it running.  You can use the curl command to access the briar-headless API for adding a contact, or you can use some [php scripts](https://github.com/Perelandra0x309/briar-w3-css/tree/master/briar-php-api) I created to make using the API easier.  For example, to add a contact using my script:
+
+```
+export AUTH_TOKEN=`cat ~/.briar/auth_token`
+php briar-cli.php --action=add_contact --link=[briar link here] --alias=[device name]
+```
+
+You can verify the contact was successfully added and send a test message with:
+
+```
+php briar-cli.php --action=list_all_contacts
+php briar-cli.php --action=write_message --contact_id=[id number] --text=[message]
+```
+
+Once you have verified your device and the briar-headless are communicating you can start the BriarBot by cloning this Github to your PC and running this command from a shell in the cloned git repository path:
+
+```
+java -jar build/libs/briarbot-0.1.0.jar
+```
+<blockquote>
+Note that currently only contact ID 1 is allowed to send messages to the BriarBot.  Messages from any other contact ID will be rejected.  If you need to change this then find the contact ID you wish to use by listing all contacts from briar-headless and looking for the "contactId" value of the contact you wish to add. Then alter the BriarBot.java class and modify the following line to define your allowed contact ID:
+
+```java
+private final int allowedContactId = 1;
+```
+
+Then build the jar with gradle:
+
+```
+gradle build
+```
+</blockquote>
+
+Once you run the briar-bot application you should see a successful connection status message:
+
+```
+briar-bot$ java -jar build/libs/briarbot-0.1.0.jar 
+Sep 09, 2019 8:12:45 PM com.briarbot.client.BriarBot onOpen
+INFO: Connected ... 414fdb51-b517-4609-8967-67a7d65a6c48
+```
+
+From your other connected device you can now send a message like this:
+
+```
+bb [command] [options or other text]
+```
+
+Right now the only commands are 'echo' and 'run'.  Using 'echo' will simply send a reply message with any of the text sent in the original message after the 'echo' command'.  Using 'run' will execute the text proceeding the 
+run command in a shell.  Any text output by the shell will be returned in a message to your device.  As an example, you can send the message "bb run date" to get the date string from your PC:
+
+![Example](/images/run_example.jpg)
+
+These are only simple examples of what can be sent to a BriarBot enabled PC from a Briar contact.  In a future update I plan to add the ability to have a repository of scripts that can be run from BriarBot. Running scipts will allow much more complicated actions to be performed with a short message from your Briar contact.
+
+Note that one restriction in place now is that the BriarBot will time out and disconnect after 5 minutes.  This version is only meant as a demonstration, not a continually running process.  In future versions this will change.
---
+++ src/main/java/com/briarbot/client/BriarBot.java
@@ -0,0 +1,181 @@
+package com.briarbot.client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URI;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Logger;
+import java.util.Map;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+
+import org.apache.commons.text.StringEscapeUtils;
+
+import org.glassfish.tyrus.client.ClientManager;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*; 
+
+
+@ClientEndpoint
+public class BriarBot {
+
+    private static CountDownLatch latch;
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+    private String bearerToken;
+    private BufferedReader br;
+    private final int allowedContactId = 1;
+    private final String botKeyword = "bb";
+
+    @OnOpen
+    public void onOpen(Session session) throws IOException {
+        logger.info("Connected ... " + session.getId());
+        try {
+        	String tokenFilePath = System.getProperty("user.home") + "/.briar/auth_token";
+        	File file = new File(tokenFilePath); 
+        	br = new BufferedReader(new FileReader(file));
+        	String authToken = br.readLine();
+        	if (authToken == null || authToken.equals("")) {
+        		logger.warning("Briar authentication token missing.  You should have a file " + tokenFilePath + " containing your Briar authentication token.");
+        		latch.countDown();
+        		return;
+        	}
+        	bearerToken = "Bearer " + authToken;
+        	session.getBasicRemote().sendText(authToken);
+        } catch (IOException e) {
+        	logger.warning(e.toString());
+            throw new RuntimeException(e);
+        } finally {
+        	br.close();
+        }
+    }
+
+    @OnMessage
+    public void onMessage(String json, Session session) {
+        try {
+            logger.info("message: " + json);
+
+    		JSONObject jo = (JSONObject) (new JSONParser().parse(json));
+    		Map data = ((Map)jo.get("data"));
+    		
+    		// Restrict by contacts allowed
+            Long contactId = (Long) data.get("contactId");
+            if (contactId != allowedContactId) { // TODO make this configurable
+            	logger.info("Bot use not allowed for contact ID " + contactId);
+            	return;
+            }
+            
+            // Only execute bot messages
+            String messageText = (String) data.get("text");
+            String[] splitMessage = messageText.split(" ", 3);
+            if (splitMessage.length == 0 || !splitMessage[0].equalsIgnoreCase(botKeyword)) {
+            //	logger.info("'" + splitMessage[0] + "'");
+            	return;
+            }
+            
+            String replyText = "";
+            if(splitMessage.length == 1 || splitMessage[1].equals("")) {
+            	replyText = "I'm here.  If you need help type 'bot help'.";
+            }
+            else {
+            	String messageRemainder = "";
+            	if(splitMessage.length == 3) {
+            		messageRemainder = splitMessage[2];
+            	}
+	            switch(splitMessage[1].toLowerCase()) {
+	            	case "echo":
+	            		replyText = "You said... " + messageRemainder;
+	            		break;
+	            	case "help":
+	            		replyText = "To do";
+	            		break;
+	            	case "run":
+	            		boolean isWindows = System.getProperty("os.name")
+	            		  .toLowerCase().startsWith("windows");
+	            		ProcessBuilder builder = new ProcessBuilder();
+	            		if (isWindows) {
+	            		    builder.command("cmd.exe", "/c", "dir");
+	            		} else {
+	            		    builder.command("sh", "-c", messageRemainder);
+	            		}
+	            		builder.directory(new File(System.getProperty("user.home") + "/.briar/bot_scripts"));
+	            		Process process = builder.start();
+	            		BufferedReader br=new BufferedReader(new InputStreamReader(process.getInputStream()));
+	            	    String line;
+	            	    while((line=br.readLine())!=null){
+	            	    	replyText += line + "\n";
+	            	    }
+	            	    int exitValue = process.waitFor();
+	            	    logger.info("\n\nExit Value is " + exitValue);
+	            	    if (exitValue != 0) {
+	            	    	replyText = "Warning, exit value=" + exitValue + " " + replyText;
+	            	    }
+	            		break;
+	            	default:
+	            		return;
+	            }
+            
+            }
+            
+            String replyJson = "{\"text\": \"" + StringEscapeUtils.escapeJava(replyText) + "\"}";
+            
+    		URL url = new URL("http://127.0.0.1:7000/v1/messages/1");
+    		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    		conn.setDoOutput(true);
+    		conn.setRequestMethod("POST");
+    		conn.setRequestProperty("Content-Type", "application/json");
+    		conn.setRequestProperty("Authorization", bearerToken);
+    		OutputStream os = conn.getOutputStream();
+    		os.write(replyJson.getBytes());
+    		os.flush();
+    		os.close();
+
+    		if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+    			throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode() + ", reply: "+replyJson);
+    		}
+    		
+    		BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    		String output;
+    		logger.info("Output from Server ....");
+    		while ((output = br.readLine()) != null) {
+    			logger.info(output);
+    		}
+
+    		conn.disconnect();
+
+        } catch (Exception e) {
+        	logger.warning(e.toString());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @OnClose
+    public void onClose(Session session, CloseReason closeReason) {
+        logger.info(String.format("Session %s close because of %s", session.getId(), closeReason));
+        latch.countDown();
+    }
+
+    public static void main(String[] args) {
+        latch = new CountDownLatch(1);
+
+        ClientManager client = ClientManager.createClient();
+        try {
+            client.connectToServer(BriarBot.class, new URI("ws://localhost:7000/v1/ws"));
+            latch.await();
+
+        } catch ( Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
